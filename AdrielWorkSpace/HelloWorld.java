fff08750d898556690a17852d65a2a7dddb0319b
adriel <62419339+ninja12445@users.noreply.github.com>
2025-03-28 08:28:47 -0400

    Add files via upload

---
+++ AdrielWorkSpace/HelloWorld.java
@@ -0,0 +1,24 @@
+/* *********************************************************************
+Name: Adriel 
+Action: A java program that outputs Hello, World! on screen
+*/
+
+/**
+ *
+ * @author tuesd
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");//
+    }
+    
+}
+
+/* **************************Output*****************************
+ Hello, World!    
+*/
+
