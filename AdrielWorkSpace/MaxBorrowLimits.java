37784fb88bb6da4c1e5034b43c6c145d76d6cff3
adriel <62419339+ninja12445@users.noreply.github.com>
2025-04-25 21:00:08 -0400

    Add files via upload

---
+++ AdrielWorkSpace/MaxBorrowLimits.java
@@ -0,0 +1,57 @@
+package max;
+
+public class MaxBorrowLimits {
+    private int maxNumOfBooksBorrowed;
+    private String[] bookNames = new String[100]; // for encapsulation & code obfuscation
+    
+   
+    public MaxBorrowLimits(String [] collection, int maxNumOfBooksBorrowed) {
+        this.maxNumOfBooksBorrowed = maxNumOfBooksBorrowed;
+        
+        if (collection != null) 
+            this.bookNames = collection;
+        else 
+            this.bookNames = new String[0];
+   
+    }
+    
+    /**
+     * Gets number of books the user can possibly borrow / withdraw from the virtual library
+     * @return max numbers of books are allowed to borrow (must not pass threshold for validation           check)
+     */
+    
+    public int getBorrowLimits(){
+        return maxNumOfBooksBorrowed; // purpose: accessing private int value
+    }
+    
+    /**
+     * Sets max number of book user can borrowed 
+     * @param l : limit
+     * pass as max from main into this 
+     */
+    public void setBorrowLimits(int l) {
+        this.maxNumOfBooksBorrowed = l; // update instance variable with new limitations.
+    }
+
+    
+    public void setBorrowLimits() {
+        // in main: automatically see this prints out when the user selected book is 0
+        if (bookNames.length == 0 || bookNames == null) {
+            System.out.println("User have not selected any books to borrow. ");
+            return;
+        }
+        
+        if (bookNames.length > this.maxNumOfBooksBorrowed) {
+            System.out.println("You are only allowed to borrow up to 5 books at the same time");
+            System.out.printf("You have borrowed %d more than limit.\n", (bookNames.length - this.maxNumOfBooksBorrowed));
+            System.out.println("Please go ahead reselecting the number of books you have chosen. ");
+        } else {
+            for (String bookName : bookNames) {
+                System.out.println(bookName);
+            }
+        }
+    }
+    
+    
+    
+}
