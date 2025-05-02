db404d688c5627939c8ca0123ec78ad892c77436
adriel <62419339+ninja12445@users.noreply.github.com>
2025-05-02 11:29:12 -0400

    Add files via upload

---
+++ AdrielWorkSpace/ManagerModule.java
@@ -0,0 +1,71 @@
+
+package max;
+
+/**
+ *
+ * @author adriel
+ */
+/*
+=============================
+Borrowed, Reserve, Returned 
+[Part 3] Book Management Unit System
+21. listAllAvailableBooks() ~ books are ready for the student to borrow
+22. listAllBorrowedBooks()  ~ book are marked as in use by student or others 
+23. getBooksDetails() ~ returns book ID
+24. batchUpdateBooks() ~ similar to fetch string find and replace, update the books with new informations  
+25. sortBookCollectionByRating()
+26. shuffleBookOrdering - randomize book order
+27. rateBook() - user can rate book 
+28. getBookReview(), getTopRatedBooks()  
+=============================
+*/
+public class ManagerModule {
+    
+    public void listAllAvailableBooks(){
+        
+    }
+    
+    public void listAllBorrowedBooks() {
+        
+    }
+    
+    public void getBooksDetails() {
+        
+    }
+    
+    public void batchUpdateBooks() {
+        
+    }
+    
+    public void sortBookCollectionByRating() {
+        
+    }
+    
+    public void shuffleBookOrdering() {
+        
+    }
+    
+    public void rateBook() {
+        
+    }
+    
+    public void getBookReview() {
+        
+    }
+    
+    public void getTopRatedBook() {
+        
+    }
+    
+    // new feature using write file 
+    public void sendNotice() {
+        
+    }
+    
+    
+    // new feature: check due date & time 
+    public void setDueDateTime() {
+     // "OVERDUE NOTICE"
+    }
+     
+}
---
+++ AdrielWorkSpace/UserRegistration.java
@@ -0,0 +1,25 @@
+
+package max;
+
+/**
+ *
+ * Copyright (C) 2025 Adriel Nguyen
+ * make it into three phrases 
+ * Phrase 1: brainstorm pseudocode
+ * Phrase 2: begin to code
+ * Phrase 3: Debug + test algorithms
+ */
+
+
+public class UserRegistration {
+    // Personal Information
+    // Name: 
+    // Department: 
+    // Mailing Address: 
+    // User Mobile phone number: 
+    // Loan method is: 
+}
+
+// password, username, email holder for registration [with validation]
+
+
